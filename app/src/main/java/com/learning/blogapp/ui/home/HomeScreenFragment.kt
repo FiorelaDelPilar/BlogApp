4cc9f9d87614f44b611918ac40167c744457044d
FIORELA DEL PILAR <fiorela2496@gmail.com>
2023-03-09 17:10:46 -0500

    S11-94 Creando ui

---
+++ app/src/main/java/com/learning/blogapp/ui/home/HomeScreenFragment.kt
@@ -0,0 +1,15 @@
+package com.learning.blogapp.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.learning.blogapp.R
+
+
+class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
