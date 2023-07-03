2878a45709473633d9d27006fd984fa5eb27eba1
fvargasi <fvargasi@emeal.nttdata.com>
2023-07-03 17:54:56 -0500

    107 Obteniendo y validando datos del registro

--- app/src/main/java/com/learning/blogapp/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/learning/blogapp/ui/auth/RegisterFragment.kt
@@ -1,6 +1,7 @@
 package com.learning.blogapp.ui.auth
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +16,44 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentRegisterBinding.bind(view)
+        signUp()
+    }
+
+    private fun signUp(){
+        binding.btnSignup.setOnClickListener{
+            val username = binding.editTextUsername.text.toString().trim()
+            val email = binding.editTextEmail.text.toString().trim()
+            val password = binding.editTextPassword.text.toString().trim()
+            val passwordConfirm = binding.editTextConfirmPassword.text.toString().trim()
+
+            if(!password.equals(passwordConfirm)){
+                binding.editTextPassword.error = "Password does not match"
+                binding.editTextConfirmPassword.error = "Password does not match"
+                return@setOnClickListener
+            }
+
+            if(username.isEmpty()){
+                binding.editTextUsername.error = "Username is empty"
+                return@setOnClickListener
+            }
+
+            if(email.isEmpty()){
+                binding.editTextEmail.error = "E-mail is empty"
+                return@setOnClickListener
+            }
+
+            if(password.isEmpty()){
+                binding.editTextPassword.error = "Password is empty"
+                return@setOnClickListener
+            }
+
+            if(passwordConfirm.isEmpty()){
+                binding.editTextConfirmPassword.error = "Password is empty"
+                return@setOnClickListener
+            }
+
+            Log.d("signUpData", "data: $username $email $password $passwordConfirm")
+        }
     }
 
 }
