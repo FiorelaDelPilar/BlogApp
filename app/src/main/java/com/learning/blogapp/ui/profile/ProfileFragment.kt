e1776e29ae1ad79edb70422dcf209124d6fa09d0
fvargasi <fvargasi@emeal.nttdata.com>
2023-07-05 23:23:05 -0500

    130 Mostrando foto y nombre en el perfil

--- app/src/main/java/com/learning/blogapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/learning/blogapp/ui/profile/ProfileFragment.kt
@@ -3,13 +3,20 @@ package com.learning.blogapp.ui.profile
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.learning.blogapp.R
+import com.learning.blogapp.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    private lateinit var binding: FragmentProfileBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentProfileBinding.bind(view)
+
         val user = FirebaseAuth.getInstance().currentUser
+        Glide.with(this).load(user?.photoUrl).centerCrop().into(binding.imgProfile)
+        binding.txtProfileName.text = user?.displayName
 
     }
 }
