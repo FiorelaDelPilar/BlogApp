4c519bc8a17d95b5991b7a4fc9102f5c00d27f35
fvargasi <fvargasi@emeal.nttdata.com>
2023-11-03 12:12:16 -0500

    152 Obteniendo datos en UI con STATEFLOW

--- app/src/main/java/com/learning/blogapp/ui/main/HomeScreenFragment.kt
+++ app/src/main/java/com/learning/blogapp/ui/main/HomeScreenFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.learning.blogapp.R
 import com.learning.blogapp.core.Result
 import com.learning.blogapp.data.model.Post
@@ -17,15 +21,17 @@ import com.learning.blogapp.presentation.HomeScreenViewModelFactory
 import com.learning.blogapp.ui.main.adapters.HomeScreenAdapter
 import com.learning.blogapp.ui.main.adapters.OnPostClickListener
 import com.learning.blogapp.utils.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 class HomeScreenFragment : Fragment(R.layout.fragment_home_screen), OnPostClickListener {
 
     private lateinit var binding: FragmentHomeScreenBinding
-    private val viewModel by viewModels<HomeScreenViewModel>{
+    private val viewModel by viewModels<HomeScreenViewModel> {
         HomeScreenViewModelFactory(
             HomeScreenRepoImpl(
-            HomeScreenDataSource()
+                HomeScreenDataSource()
             )
         )
     }
@@ -34,66 +40,110 @@ class HomeScreenFragment : Fragment(R.layout.fragment_home_screen), OnPostClickL
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeScreenBinding.bind(view)
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.latestPosts.collect { result ->
+                    when (result) {
+                        is Result.Loading -> {
+                            binding.progressBar.show()
+                        }
+
+                        is Result.Success -> {
+                            binding.progressBar.hide()
+                            if (result.data.isEmpty()) {
+                                binding.emptyContainer.show()
+                                return@collect
+                            } else {
+                                binding.emptyContainer.hide()
+                            }
+                            binding.rvHome.adapter = HomeScreenAdapter(result.data, this@HomeScreenFragment)
+                        }
+
+                        is Result.Failure -> {
+                            binding.progressBar.hide()
+                            Toast.makeText(
+                                requireContext(),
+                                "Ocurrió un error: ${result.exception}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+                    }
+                }
+            }
+        }
+
+        /*
+          val postList = listOf(
+              Post(
+                  "https://i.pinimg.com/236x/85/99/ea/8599ea1d4454394617df83d48cb0f218.jpg",
+                  "Tara Grem",
+                  Timestamp.now(),
+                  "https://i.pinimg.com/236x/39/62/54/396254cab60fe9c8313ad6195ab861f2.jpg"
+              ),
+              Post(
+                  "https://i.pinimg.com/236x/f4/6d/39/f46d39518ca726b2a177614e2b3ca3a1.jpg",
+                  "Ethan Trest",
+                  Timestamp.now(),
+                  "https://i.pinimg.com/236x/90/5a/55/905a5534acc69debf3f4d5ec5de5ab92.jpg"
+              ),
+              Post(
+                  "https://i.pinimg.com/236x/4b/56/e0/4b56e05a9d97331c7cc26716f0a0ffdd.jpg",
+                  "Ian Trest",
+                  Timestamp.now(),
+                  "https://i.pinimg.com/236x/58/9d/90/589d902a331fa4c6bf3fe9d63c690f61.jpg"
+              )
+          )
+         */
       /*
-        val postList = listOf(
-            Post(
-                "https://i.pinimg.com/236x/85/99/ea/8599ea1d4454394617df83d48cb0f218.jpg",
-                "Tara Grem",
-                Timestamp.now(),
-                "https://i.pinimg.com/236x/39/62/54/396254cab60fe9c8313ad6195ab861f2.jpg"
-            ),
-            Post(
-                "https://i.pinimg.com/236x/f4/6d/39/f46d39518ca726b2a177614e2b3ca3a1.jpg",
-                "Ethan Trest",
-                Timestamp.now(),
-                "https://i.pinimg.com/236x/90/5a/55/905a5534acc69debf3f4d5ec5de5ab92.jpg"
-            ),
-            Post(
-                "https://i.pinimg.com/236x/4b/56/e0/4b56e05a9d97331c7cc26716f0a0ffdd.jpg",
-                "Ian Trest",
-                Timestamp.now(),
-                "https://i.pinimg.com/236x/58/9d/90/589d902a331fa4c6bf3fe9d63c690f61.jpg"
-            )
-        )
-       */
-        viewModel.fetchLatestPosts().observe(viewLifecycleOwner, Observer {result->
-            when(result){
+        viewModel.fetchLatestPosts().observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
                 is Result.Loading -> {
                     binding.progressBar.show()
                 }
 
-                is Result.Success ->{
+                is Result.Success -> {
                     binding.progressBar.hide()
-                    if(result.data.isEmpty()){
+                    if (result.data.isEmpty()) {
                         binding.emptyContainer.show()
                         return@Observer
-                    }else{
+                    } else {
                         binding.emptyContainer.hide()
                     }
                     binding.rvHome.adapter = HomeScreenAdapter(result.data, this)
                 }
 
-                is Result.Failure ->{
+                is Result.Failure -> {
                     binding.progressBar.hide()
-                    Toast.makeText(requireContext(), "Ocurrió un error: ${result.exception}", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        requireContext(),
+                        "Ocurrió un error: ${result.exception}",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
 
             }
         })
 
+       */
         //binding.rvHome.adapter = HomeScreenAdapter(postList)
 
     }
 
     override fun onLikeButtonClick(post: Post, liked: Boolean) {
-        viewModel.registerLikeButtonState(post.id, liked).observe(viewLifecycleOwner, Observer {result->
-            when(result){
-                is Result.Loading -> {}
-                is Result.Success ->{}
-                is Result.Failure ->{
-                    Toast.makeText(requireContext(), "Ocurrió un error: ${result.exception}", Toast.LENGTH_SHORT).show()
+        viewModel.registerLikeButtonState(post.id, liked)
+            .observe(viewLifecycleOwner, Observer { result ->
+                when (result) {
+                    is Result.Loading -> {}
+                    is Result.Success -> {}
+                    is Result.Failure -> {
+                        Toast.makeText(
+                            requireContext(),
+                            "Ocurrió un error: ${result.exception}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-            }
-        })
+            })
     }
 }
