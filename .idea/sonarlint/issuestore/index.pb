b6df581b9f0818e4755b87eea18ddb31ba92b1d9
fvargasi <fvargasi@emeal.nttdata.com>
2023-11-03 11:42:45 -0500

    150-151 ViewModel STATEFLOW

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+o
+?app/src/main/java/com/learning/blogapp/ui/auth/LoginFragment.kt,3/e/3eec486dfbd64700f71f32c0d8d68e4cbe8f76ad
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
--- app/src/main/java/com/learning/blogapp/presentation/main/HomeScreenViewModel.kt
+++ app/src/main/java/com/learning/blogapp/presentation/main/HomeScreenViewModel.kt
@@ -5,36 +5,55 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import com.learning.blogapp.core.Result
+import com.learning.blogapp.data.model.Post
 import com.learning.blogapp.domain.home.HomeScreenRepo
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 import java.lang.Exception
 
-class HomeScreenViewModel(private val repo: HomeScreenRepo): ViewModel() {
-    fun fetchLatestPosts() = liveData(Dispatchers.IO){
+class HomeScreenViewModel(private val repo: HomeScreenRepo) : ViewModel() {
+    fun fetchLatestPosts() = liveData(Dispatchers.IO) {
         emit(Result.Loading())
         kotlin.runCatching {
             repo.getLatestPosts()
-        }.onSuccess {postList->
+        }.onSuccess { postList ->
             emit(postList)
         }.onFailure {
             emit(Result.Failure(Exception(it.message)))
         }
     }
 
-    fun registerLikeButtonState(postId: String, liked: Boolean) = liveData(viewModelScope.coroutineContext + Dispatchers.Main) {
-        emit(Result.Loading())
-        kotlin.runCatching {
-            repo.registerLikeButtonState(postId, liked)
-        }.onSuccess {
-            emit(Result.Success(Unit))
-        }.onFailure {
-            emit(Result.Failure(Exception(it.message)))
+    fun registerLikeButtonState(postId: String, liked: Boolean) =
+        liveData(viewModelScope.coroutineContext + Dispatchers.Main) {
+            emit(Result.Loading())
+            kotlin.runCatching {
+                repo.registerLikeButtonState(postId, liked)
+            }.onSuccess {
+                emit(Result.Success(Unit))
+            }.onFailure {
+                emit(Result.Failure(Exception(it.message)))
+            }
         }
-    }
 
+    val latestPosts : StateFlow<Result<List<Post>>> = flow {
+        kotlin.runCatching {
+            repo.getLatestPosts()
+        }.onSuccess { resultPostList ->
+            emit(resultPostList)
+        }.onFailure { throwable ->
+            emit(Result.Failure(Exception(throwable)))
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = Result.Loading()
+    )
 }
 
-class HomeScreenViewModelFactory(private val repo: HomeScreenRepo): ViewModelProvider.Factory{
+class HomeScreenViewModelFactory(private val repo: HomeScreenRepo) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(HomeScreenRepo::class.java).newInstance(repo)
     }
